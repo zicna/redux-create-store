7e092463dc453d905b3eb51ea8bd909d2026ff93
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-11-17 10:36:20 -0600

    commit tree 5bb3a14d39d04bf730e8f64a08ce9cce4bbafa84

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,19 @@
-let state;
+function createStore(reducer){
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
+
+  function getState(){
+    return state
+  }
+
+  return {
+    dispatch, getState
+  }
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +25,18 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+let store = createStore(reducer)
+store.dispatch({type:'@@INIT'})
+
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
+
